7505413909040ee806d0039f61598e508d4832a3
heku777 <chotips08@gmail.com>
2021-07-06 13:00:16 +0900

    #19 heroku deploy

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,7 +134,6 @@ if DEBUG:
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if not DEBUG:
-    import dj_database_url
 
     ALLOWED_HOSTS = ["outpro.herokuapp.com"]
 
